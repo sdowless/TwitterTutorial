eaf4c1b5b936907b3750e20d7fa0615a957cd70d
Stephan Dowless <dowless.stephan@gmail.com>
2020-01-28 15:12:58 -0500

    Left bar button

--- TwitterTutorial/Controller/FeedController.swift
+++ TwitterTutorial/Controller/FeedController.swift
@@ -7,13 +7,14 @@
 //
 
 import UIKit
+import SDWebImage
 
 class FeedController: UIViewController {
     
     // MARK: - Properties
     
     var user: User? {
-        didSet { print("DEBUG: Did set user in feed controller..") }
+        didSet { configureLeftBarButton() }
     }
     
     // MARK: - Lifecycle
@@ -31,11 +32,17 @@ class FeedController: UIViewController {
         let imageView = UIImageView(image: UIImage(named: "twitter_logo_blue"))
         imageView.contentMode = .scaleAspectFit
         navigationItem.titleView = imageView
+    }
+    
+    func configureLeftBarButton() {
+        guard let user = user else { return }
         
         let profileImageView = UIImageView()
-        profileImageView.backgroundColor = .twitterBlue
         profileImageView.setDimensions(width: 32, height: 32)
         profileImageView.layer.cornerRadius = 32 / 2
+        profileImageView.layer.masksToBounds = true
+        
+        profileImageView.sd_setImage(with: user.profileImageUrl, completed: nil)
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(customView: profileImageView)
     }
--- TwitterTutorial/Model/User.swift
+++ TwitterTutorial/Model/User.swift
@@ -12,7 +12,7 @@ struct User {
     let fullname: String
     let email: String
     let username: String
-    let profileImageUrl: String
+    var profileImageUrl: URL?
     let uid: String
     
     init(uid: String, dictionary: [String: AnyObject]) {
@@ -21,6 +21,11 @@ struct User {
         self.fullname = dictionary["fullname"] as? String ?? ""
         self.email = dictionary["email"] as? String ?? ""
         self.username = dictionary["username"] as? String ?? ""
-        self.profileImageUrl = dictionary["profileImageUrl"] as? String ?? ""
+        
+        if let profileImageUrlString = dictionary["profileImageUrl"] as? String {
+            guard let url = URL(string: profileImageUrlString) else { return }
+            self.profileImageUrl = url
+        }
+        
     }
 }
